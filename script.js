ca07900542ffa6a9da357b5d9ec0b2ee57d08044
jonas <jonas.langenberg@outlook.de>
2024-07-26 18:35:51 +0200

    Add functionality to include points in numbers

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+
+
 const allNumbers = document.querySelectorAll(".number");
 let currentOperation = {valueOne: 0, valueTwo: 0, operator: 0, result: 0,};
 for(let number of allNumbers){
@@ -30,4 +33,29 @@ for(let operator of allOperators){
         }
     });
 }
+
+const myPoint = document.querySelector("#point");
+
+myPoint.addEventListener("click", () => {
+    let checkOneForPoint = currentOperation.valueOne.toString();
+    let checkTwoForPoint = currentOperation.valueTwo.toString();
+    if(currentOperation.valueOne == 0){
+        currentOperation.valueOne = "0.";
+        console.log(currentOperation);
+    } else if(currentOperation.valueTwo == 0 && currentOperation.operator != 0){
+        currentOperation.valueTwo = "0.";
+        console.log(currentOperation);
+    } else if(checkOneForPoint.includes(".") && currentOperation.operator == 0){
+        alert("There already is a point in that value!");
+    }else if(checkTwoForPoint.includes(".") && currentOperation.operator != 0){
+        alert("There already is a point in that value!");
+    }else if (currentOperation.valueOne != 0 && currentOperation.operator == 0){
+        currentOperation.valueOne += myPoint.value;
+        console.log(currentOperation);
+    } else {
+        currentOperation.valueTwo += myPoint.value;
+        console.log(currentOperation);
+    }
+});
+
 
